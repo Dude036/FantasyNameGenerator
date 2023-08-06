7779e34bb9e5a64eae0e57ec20d3bbffc9aa66f7
Josh Higham <joshua.higham@collectivemedicaltech.com>
2023-08-06 12:58:11 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Fantasy Names
+A Python project dedicated to generating names from various bits of fantasy
+
+## Installation
+
+TODO: Add this to PyPi
+
+## Usage
+
+TODO: Describe usage here
+
+Note: Add a thing here about using a formatting tool to simplify formatting
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='FantasyNameGenerator',
+    version='0.0.1',
+    install_requires=[],
+    classifiers=[
+        'Private :: Do Not Upload', # PyPI will reject any package uploads that has any classifier with the 'Private ::' prefix
+        'Programming Language :: Python :: 3 :: Only'
+    ],
+)
